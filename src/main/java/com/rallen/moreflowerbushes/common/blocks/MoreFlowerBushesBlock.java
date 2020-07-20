2356160b3f3a4e60252ac8c7470f69eceb91693d
Aidan <aidanpauldavis@yahoo.co.uk>
2020-07-20 17:15:03 +0100

    got them displaying with light, just the white texture issue now

--- src/main/java/com/rallen/moreflowerbushes/common/blocks/MoreFlowerBushesBlock.java
+++ src/main/java/com/rallen/moreflowerbushes/common/blocks/MoreFlowerBushesBlock.java
@@ -1,20 +1,28 @@
 package com.rallen.moreflowerbushes.common.blocks;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.block.DoublePlantBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
 
 public class MoreFlowerBushesBlock extends DoublePlantBlock {
     private static Properties props = Properties.create(Material.TALL_PLANTS)
-            .hardnessAndResistance(0.5f)
+            .hardnessAndResistance(0f)
             .sound(SoundType.PLANT)
             .doesNotBlockMovement()
-            .variableOpacity()
-            .notSolid()
+            .tickRandomly()
             .harvestLevel(0);
     protected MoreFlowerBushesBlock(String registryName) {
         super(props);
         setRegistryName(registryName);
 
     }
+
+    @Override
+    public boolean propagatesSkylightDown(BlockState state, IBlockReader reader, BlockPos pos) {
+        return true;
+    }
+
 }
